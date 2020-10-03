da9db6830ec3609b8f00beefec5e8f77725724f3
Greg Rashkevitch <rashkevitch@gmail.com>
2020-10-03 12:47:40 +0300

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,36 @@
 import { HttpStatus } from './lib/responses';
 import { NextFunction, Request, Response } from 'express';
 
-export declare enum Modes {
+export = ChaoticResponse;
+
+declare enum Modes {
   OPTIMISTIC = 'optimistic',
   PESSIMISTIC = 'pessimistic',
   TIMEOUT = 'timeout',
   FAILURE = 'failure'
 }
 
-export declare interface ChaoticModes {
-  responses: HttpStatus[],
-  weights: number[]
-}
-
-export declare interface ChaoticOptions {
-  mode: Modes,
-  customMode: ChaoticModes,
-  timeout: number
-}
-
-
-export declare class ChaoticResponse {
-  constructor(options: ChaoticOptions);
+declare class ChaoticResponse {
+  constructor(options?: ChaoticResponse.ChaoticOptions);
 
   setMode(mode: Modes): void;
-  getMode(): ChaoticModes;
+
+  getMode(): ChaoticResponse.ChaoticModes;
+
   callbackOnError: () => void;
+
   middleware(req: Request, res: Response, next: NextFunction): void
 }
 
-export declare function ChaoticResponse(options: ChaoticOptions): void
+declare namespace ChaoticResponse {
+  type ChaoticModes = {
+    responses: HttpStatus[],
+    weights: number[]
+  }
+
+  type ChaoticOptions = {
+    mode: Modes,
+    customMode: ChaoticResponse.ChaoticModes,
+    timeout: number
+  }
+}
